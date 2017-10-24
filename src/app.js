56d5be7ab76ffd1c98ffaae130720a601af67267
Rokas <rokas@imrokas.com>
2017-10-24 10:59:03 -0700

    scaleTime and working with temporal data

--- src/app.js
+++ src/app.js
@@ -1,14 +1,9 @@
-const linearScale = d3.scaleLinear()
-	.domain([0, 100]) // input domain, values that we are expecting to get
-	.range([0, 600]) // range to map the values to
-	.clamp(true); // clamp value so it would always fit the given range
+// working with Dates and time scales, when plotting temporal data
+const timeScale = d3.scaleTime()
+	.domain([new Date(2017, 0, 1), new Date()]) // define date possible range
+	.range([0, 100]) // range that values will plot against
 
-console.log(linearScale(-50))
-console.log(linearScale(0))
-console.log(linearScale(25))
-console.log(linearScale(50))
-console.log(linearScale(75))
-console.log(linearScale(100))
-console.log(linearScale(150))
-
-console.log(linearScale.invert(300))
+console.log(timeScale(new Date(2017, 0, 15)))
+console.log(timeScale(new Date(2017, 4, 15)))
+console.log(timeScale(new Date()))
+console.log(timeScale.invert(50)) // invert to get exactly middle date for the given domain
