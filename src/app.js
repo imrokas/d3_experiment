2c36156667f18e076edf79694803270eb434ab55
Rokas <rokas@imrokas.com>
2017-10-24 16:22:13 -0700

    add axis to chart

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-// dealing with margin d3 way
+// ADDING AXIS TO THE CHART
 const margin = {
-	top: 0,
+	top: 25,
 	right: 0,
-	bottom: 25,
-	left: 25
+	bottom: 30,
+	left: 30
 };
 // calculate width, height for inner elements,
 // there will be no need to worry about margins
@@ -22,3 +22,24 @@ svg.append('rect')
 	.attr('height', height)
 	.style('fill', 'lightblue')
 	.style('stroke', 'green')
+
+const yScale = d3.scaleLinear()
+	.domain([0, 100]) // defines y axis scale values
+	.range([height, 0]); // because y goes in reverse in svg's
+
+const yAxis = d3.axisLeft(yScale) // create y axis
+	//.ticks(5, ); // amount of ticks to show on axis, second param label
+	//.tickValues([8, 17, 29, 78]) // custom tick values
+svg.call(yAxis); // add axis to chart
+
+const xScale = d3.scaleTime()
+	.domain([new Date(2017, 9, 24), new Date(2017, 10, 24)])
+	.range([0, width]);
+
+const xAxis = d3.axisBottom(xScale)
+	.ticks(5);
+
+svg
+	.append('g')
+		.attr('transform', `translate(0, ${height})`)
+	.call(xAxis)
