cfde69c2247e38c1790540d4b6d33acc3bf44fd7
Rokas <rokas@imrokas.com>
2017-10-24 15:50:02 -0700

    chaining calls using .call

--- src/app.js
+++ src/app.js
@@ -18,6 +18,18 @@ const bar = d3.select('.chart')
 		.attr('transform', (d, i) => `translate(0, ${i * 33})`);
 		// graphic container doesn't have typical y prop
 
+const scaleBar = (selection, scale) => {
+	selection.style('transform', `scaleX(${scale})`);
+}
+
+const fade = (selection, opacity) => {
+	selection.style('fill-opacity', opacity);
+}
+
+const setFill = (selection, color) => {
+	selection.style('fill', color);
+}
+
 // add rectangles to g containers
 bar.append('rect')
 		.style('width', d => d.score)
@@ -25,11 +37,20 @@ bar.append('rect')
 		// equivalent to :hover
 		.on('mouseover', function(d, i, elements) { // need to use function to have access to this
 			d3.select(this)
-				.classed('barOn', true)
+				.call(scaleBar, 1.5)
+				.call(setFill, 'orange');
+				
+			d3.selectAll(elements)
+				.filter(':not(:hover)')
+				.call(fade, 0.5);
 		})
 		.on('mouseout', function(d, i, elements) {
 			d3.select(this)
-				.classed('barOn', false)
+				.call(scaleBar, 1)
+				.call(setFill, 'lightgreen');
+
+			d3.selectAll(elements)
+				.call(fade, 1);
 		});
 
 // add text to each rect
