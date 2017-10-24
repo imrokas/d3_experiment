cb1551884bd163a5b19ac71d7f1957b0053b3f9d
Rokas <rokas@imrokas.com>
2017-10-24 12:58:27 -0700

    creating new DOM elements

--- src/app.js
+++ src/app.js
@@ -1,18 +1,7 @@
-// MODIFYING DOM ELEMENTS
-const secondLink = d3.select('a:nth-child(2');
-// get attribute value
-console.log(secondLink.attr('href'));
-// set attribute
-secondLink.attr('href', 'https://www.google.com');
-console.log(secondLink.attr('href'));
+// CREATE DOM ELEMENTS
+// .append, .insert
+d3.select('.title')
+	.insert('button', 'a:nth-child(2)') // changes the selection to new appended element
+	.html('Inventory <b>SALE</b>');
 
-// modify css styles
-// secondLink.style('color', 'red');
-
-// add css class
-secondLink.classed('red', true)
-
-// plain text
-secondLink.text('Inventory')
-// html text
-secondLink.html('Product <b>SALE</b>')
+// remove() to remove element
