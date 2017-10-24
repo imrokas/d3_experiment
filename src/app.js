ca45255319c128b4b02f7fca0a99bd592b58524d
Rokas <rokas@imrokas.com>
2017-10-24 11:10:34 -0700

    scaleQuantize, working with discrete intervals of data

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
-// working with Dates and time scales, when plotting temporal data
-const timeScale = d3.scaleTime()
-	.domain([new Date(2017, 0, 1), new Date()]) // define date possible range
-	.range([0, 100]) // range that values will plot against
+// Sometimes data needs to be converted from a continuous range, like test scores,
+// to a discrete set of output values, like letter grades. 
 
-console.log(timeScale(new Date(2017, 0, 15)))
-console.log(timeScale(new Date(2017, 4, 15)))
-console.log(timeScale(new Date()))
-console.log(timeScale.invert(50)) // invert to get exactly middle date for the given domain
+const quantizeScale = d3.scaleQuantize()
+	.domain([0, 100])
+	.range(["red", "white", "blue", "green"]); // define discrete intervals where values will live in
+
+console.log(quantizeScale(22))
+console.log(quantizeScale(45))
+console.log(quantizeScale(50))
+console.log(quantizeScale(88))
+console.log(quantizeScale(-150)) // values below domain range will fall to first discrete interval
+console.log(quantizeScale(150)) // values above domain range will fall to last discrete interval
+console.log(quantizeScale.invertExtent("white")) // range will [x, y)
