9780553316d065668f685b1414dd4aa0b85167a0
Rokas <rokas@imrokas.com>
2017-10-25 13:11:55 -0700

    reusable transitions

--- src/app.js
+++ src/app.js
@@ -1,14 +1,41 @@
-d3.select('.block')
-	.transition()
-		.duration(500)
-		.delay(750)
-		.ease(d3.easeBounceOut)
-		.style('width', '400px')
-	.transition()
-		.duration(500)
-		.ease(d3.easeBounceOut)
-		.style('height', '600px')
-	.transition()
-		.duration(2000)
-		.ease(d3.easeQuadOut)
-		.style('background-color', 'darkgreen');
+// gets destroyed after 1s after page is loaded
+// so if one wants to use when it's need to move to inside
+// of the function, or create factory function
+// const trans = d3.transition()
+// 		.delay(1000)
+// 		.duration(1000);
+
+
+const go = () => {
+	// move in to execute just during call time
+	const trans = d3.transition()
+		.delay(1000)
+		.duration(1000);
+
+	d3.selectAll('.block')
+		.transition(trans)
+		.style('width', '400px');
+
+	d3.select('.a')
+		.transition(trans)
+		.style('background-color', 'orange');
+
+	d3.select('.b')
+		.transition(trans)
+		.style('background-color', 'forestgreen');
+}
+
+// create transition config function
+const configureTransition = (trans, delay, duration) => (
+	trans
+		.delay(delay)
+		.duration(duration)
+);
+
+const goNow = () => {
+	console.log('GO NOW CALLED')
+	d3.selectAll('.block')
+		.transition()
+		.call(configureTransition, 1000, 1000)
+		.style('height', '300px');
+}
